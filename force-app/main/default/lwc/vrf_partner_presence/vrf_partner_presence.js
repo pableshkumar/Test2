5e6137301d250df31fb70fbd54c22f541fe614ca
jatinder-dev <jatinder.kumar@manras.com>
2021-01-08 16:51:12 +0530

    vendor onboarding changes in parthner

--- force-app/main/default/lwc/vrf_partner_presence/vrf_partner_presence.js
+++ force-app/main/default/lwc/vrf_partner_presence/vrf_partner_presence.js
@@ -1,4 +1,18 @@
 import { LightningElement, api, track } from 'lwc';
+import VENDOR_PARTNER_Object from '@salesforce/schema/Vendor_Partner_Presence__c';
+import COMPNAY_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Company__c';
+import CAPABILITY_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Capability__c';
+import COUNTRY_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Name_of_Country_Directly_Supported__c';
+import DESKTOP_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.DesktopLaptop_Spares__c';
+import PRINTER_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Printer_MFD_Support__c';
+import NW_EUIPMENT_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.NW_Equipment_Support__c';
+import DATA_CENTER_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Data_Center_H_F_Support__c';
+import TAPE_MANAGEMENT_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Tape_Management_Support__c';
+import AUDIO_VIDEO_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Audio_Video_Support__c';
+import VOICE_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Voice_Telephony_Support__c';
+import DC_BF_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.DC_BF_Support_for_Server_Equipment__c';
+import TELECOM_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.Telecom_Equipment__c';
+import WAREHOUSE_FIELD from '@salesforce/schema/Vendor_Partner_Presence__c.WarehouseSpare_Management__c';
 
 export default class Vrf_partner_presence extends LightningElement {
     @api accountId;
@@ -23,6 +37,29 @@ export default class Vrf_partner_presence extends LightningElement {
         });
         this.dispatchEvent(selectEvent);
     }
-
+// This is jatinder kumar code 07-01-2021
+    @track partnerList = []; 
+    @track index = 0;
+    @api recordId;
+    @track compnay = COMPNAY_FIELD ;
+    @track capability = CAPABILITY_FIELD;
+    @track nameofCountryDirectly = COUNTRY_FIELD;
+    desktopLaptopSupport=DESKTOP_FIELD;
+    desktopLaptopSpares='';
+    printerMFDSupport=PRINTER_FIELD;
+    nWEquipmentSupport=NW_EUIPMENT_FIELD;
+    dataCenter=DATA_CENTER_FIELD;
+    tapeManagement=TAPE_MANAGEMENT_FIELD;
+    voiceTelephony=VOICE_FIELD;
+    audioVideo=AUDIO_VIDEO_FIELD;
+    dCBFSupport=DC_BF_FIELD;
+    telecomEquipment=TELECOM_FIELD;
+    warehouseSpare=WAREHOUSE_FIELD;
+    partner = {
+        Name : this.name,
+        AccountNumber : this.accNumber,
+        Phone : this.phone ? this.phone : "",
+        key : ''
+    }
     
 }
